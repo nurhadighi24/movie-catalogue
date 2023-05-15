48f1a357975820a548c591e25aab0e6caa5e70ef
nurhadighi24 <nurhadi.ghifari24@gmail.com>
2023-05-15 18:36:02 +0700

    ubah API KEY

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
+  KEY: '35eb151e3eedf7e476cca1901b15543d',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
